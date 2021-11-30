0ae9df32c22fd39ada9648ad6a2b70c08a2bdfd8
jetstar7872000@gmail.com <sho@ibukishounoMacBook-Pro.local>
2021-12-01 02:44:01 +0900

    peak pattern LED

--- Arduino_AMN22112/Arduino_AMN22112.ino
+++ Arduino_AMN22112/Arduino_AMN22112.ino
@@ -1,7 +1,13 @@
 //this program made for AMN22112 Panasonic
 
+int HIGH_LED = 2;
+int LOW_LED = 3;
+
 void setup() {
   Serial.begin(9600);
+
+  pinMode(HIGH_LED, OUTPUT);
+  pinMode(LOW_LED, OUTPUT);
 }
 
 void loop() {
@@ -14,6 +20,19 @@ void loop() {
   //make vertical line per one sec
   Serial.print(millis()/1000%2*1600);
   Serial.print(",");
+
+  if (motionSensor > 600){
+    digitalWrite(HIGH_LED, HIGH);
+  } else {
+    digitalWrite(HIGH_LED, LOW);
+  }
+
+  if (motionSensor < 300){
+    digitalWrite(LOW_LED, HIGH);
+  } else {
+    digitalWrite(LOW_LED, LOW);
+  }
   
   delay(50);
+  
 }
