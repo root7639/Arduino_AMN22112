fcfa016cbaa053f1880dfd00431423d287115946
jetstar7872000@gmail.com <sho@ibukishounoMacBook-Pro.local>
2021-12-01 01:46:54 +0900

    first commit

---
+++ Arduino_AMN22112/Arduino_AMN22112.ino
@@ -0,0 +1,18 @@
+//this program made for AMN22112 Panasonic
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  
+  int motionSensor;
+  motionSensor = analogRead(A0);
+  
+  Serial.println(motionSensor);
+
+  Serial.print(millis()/1000%2*1600);
+  Serial.print(",");
+  
+  delay(50);
+}
